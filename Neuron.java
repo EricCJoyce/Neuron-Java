f7d0cf4e097ead9345252d8fd48c3296506c69fe
EricCJoyce <50152689+EricCJoyce@users.noreply.github.com>
2020-05-16 02:26:34 -0400

    Create Neuron.java

---
+++ Neuron.java
@@ -0,0 +1,15 @@
+/* Place holder Java file */
+
+import java.util.ArrayList;
+import java.io.*;
+import java.lang.Math;
+import java.lang.StringBuilder;
+
+public class NeuralNet
+  {
+    /****************************************************************/
+    //  Constructors
+    public NeuralNet()
+      {
+      }
+  }
